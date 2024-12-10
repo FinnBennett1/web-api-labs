e6df659a566aa0953d68cde9ffd38ee0a194ae7a
FinnBennett1 <122983686+FinnBennett1@users.noreply.github.com>
2024-12-10 13:55:16 +0000

    initialise dev env -data

---
+++ movies-api/initialise-dev/initDevDB.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'; // Make sure imports are correct
+import User from './models/User.js';
+import Movie from './models/Movie.js';
+import users from './seedData/users.js';
+import movies from './seedData/movies.js';
+
+async function main() {
+    if (process.env.NODE_ENV !== 'development') {
+        console.log('This script is only for the development environment.');
+        return;
+    }
+    await mongoose.connect(process.env.MONGO_DB);
+    // Drop collections
+    await User.collection.drop().catch(err => console.log('User collection not found'));
+    await Movie.collection.drop().catch(err => console.log('Movie collection not found'));
+    await User.create(users);
+    await Movie.create(movies);
+    console.log('Database initialised');
+    console.log(`${users.length} users loaded`);
+    console.log(`${movies.length} movies loaded`);
+    await mongoose.disconnect();
+}
+
+main();
